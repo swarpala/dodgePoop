b0083af36f0d26cfdb953a22f73a491e5ca17712
swarpala <85331518+swarpala@users.noreply.github.com>
2022-12-10 20:26:21 +0900

    Add files via upload

--- dodgePoop.js
+++ dodgePoop.js
@@ -5,28 +5,47 @@ document.addEventListener('DOMContentLoaded',ev=>{
     const ctx = canvas.getContext('2d');
     //------------------------------------
     class Player {
-        constructor(side = 15, speed = 3){
-            this.side = side;
+        constructor(size = 25, speed = 5){
+            this.size = size;
             this.x = canvas.width/2,
-            this.y = canvas.height-side
+            this.y = canvas.height-size
             this.speed = speed;
-            this.color = '#000000';
-            this.flashDistance = 50;
+            this.color = 'rgb(50, 160, 250)';
+            this.flashDistance = 75;
             this.flashCool = 300;
             this.isFlashEnabled = true
+            this.lookingDir = null;
             this.life = 3;
+            this.invi = false;
+            this.inviTime = 500;
+            this.damagedColor = {
+                red: 255,
+                green: 0,
+                blue: 0
+            }
+            this.flashedColor = {
+                red: 150,
+                green: 150,
+                blue: 150
+            }
+            this.idleColor = {
+                red: 50,
+                green: 160,
+                blue: 250
+            }
+            this.colorFade = 1;
         }
         
         flash() {
             if(!this.isFlashEnabled) return;
             if(rightPressed && !leftPressed){
-                if(!isInOfCanvas('width', this.x, this.side, this.flashDistance)){
-                    this.x = canvas.width - this.speed - this.side;
+                if(!isInOfCanvas('width', this.x, this.size, this.flashDistance)){
+                    this.x = canvas.width - this.speed - this.size;
                 } else {
                     this.x += this.flashDistance;
                 }
             } else if(leftPressed && !rightPressed)
-                if(!isInOfCanvas('width', this.x, this.side, this.flashDistance*(-1))){
+                if(!isInOfCanvas('width', this.x, this.size, this.flashDistance*(-1))){
                     this.x = this.speed;
                 } else {
                     this.x -= this.flashDistance;
@@ -34,16 +53,39 @@ document.addEventListener('DOMContentLoaded',ev=>{
             this.isFlashEnabled = false;
             setTimeout(()=>this.isFlashEnabled = true,this.flashCool);
         }
+
+        damaged(){
+            if (this.invi) return;
+            this.life--;
+            if(!this.life){
+                alert(`Your score is ${renderScore}. Try again!`)
+                window.location.reload();
+            }
+            this.colorFade = 0;
+            this.invi = true;
+            setTimeout(()=>this.invi = false,this.inviTime);
+        }
     }
 
     class Poop {
-        constructor(side = 15, acc = 0.1){
-            this.side = side;
-            this.x = parseInt(Math.random()*(canvas.width - side));
+        constructor(size = 15, acc = 0.1){
+            this.size = size;
+            this.x = parseInt(Math.random()*(canvas.width - size));
             this.y = 0;
             this.speed = 1;
             this.acc = acc;
             this.color = '#64320A';
+        }
+
+        iscollided(player){
+            if(
+                ((player.x >= this.x && player.x <= this.x+this.size) ||
+                (player.x+player.size >= this.x && player.x+player.size <= this.x+this.size)) &&
+                (player.y <= this.y+this.size)
+            ){
+                return true;
+            }
+            return false;
         }
     }
 
@@ -51,6 +93,7 @@ document.addEventListener('DOMContentLoaded',ev=>{
     let score = 0, renderScore = 0;
     let poopArray = [new Poop],idxOfFallenPoop = [];
     let rightPressed = false, leftPressed = false;
+    let animateId;
 
     function draw(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -59,25 +102,41 @@ document.addEventListener('DOMContentLoaded',ev=>{
         drawPoop();
         addScore();
         drawScore();
-        requestAnimationFrame(draw);
+        drawLife();
+        if(player.invi){
+            player.color = `rgb(${player.damagedColor.red}, ${player.damagedColor.green}, ${player.damagedColor.blue})`
+        } else if(!player.isFlashEnabled){
+            player.color = `rgb(${player.flashedColor.red}, ${player.flashedColor.green}, ${player.flashedColor.blue}`
+        } else {
+            player.color = `rgb(${player.idleColor.red}, ${player.idleColor.green}, ${player.idleColor.blue})`
+        }
+        animateId = requestAnimationFrame(draw);
     }
 
     function drawPlayer(){
         ctx.beginPath();
-        ctx.rect(player.x, player.y, player.side, player.side);
+        ctx.rect(player.x, player.y, player.size, player.size);
         ctx.fillStyle = player.color;
         ctx.fill();
 
-        if(rightPressed && isInOfCanvas('width', player.x, player.side, player.speed))
+        if(rightPressed && isInOfCanvas('width', player.x, player.size, player.speed))
             player.x += player.speed;
-        if(leftPressed && isInOfCanvas('width', player.x, player.side, player.speed*(-1)))
+            player.lookingDir = 'right';
+        if(leftPressed && isInOfCanvas('width', player.x, player.size, player.speed*(-1)))
             player.x -= player.speed;
+            player.lookingDir = 'left';
     }
 
     function drawScore(){
         ctx.font = '16px Consolas';
         ctx.fillStyle = '#000000';
-        ctx.fillText('점수:'+renderScore, 8, 20)
+        ctx.fillText(`score:${renderScore}`, 8, 20)
+    }
+
+    function drawLife(){
+        ctx.font = '16px Consolas';
+        ctx.fillStyle = '#000000';
+        ctx.fillText(`life:${player.life}`, 8, 40)
     }
 
     function addScore(){
@@ -90,13 +149,17 @@ document.addEventListener('DOMContentLoaded',ev=>{
         for(let i=0; i<poopArray.length; i++){
             poop = poopArray[i];
             ctx.beginPath();
-            ctx.rect(poop.x, poop.y, poop.side, poop.side);
+            ctx.rect(poop.x, poop.y, poop.size, poop.size);
             ctx.fillStyle = poop.color;
             ctx.fill();
             poop.speed += poop.acc;
             poop.y += poop.speed;
-            if(!isInOfCanvas('height', poop.y, poop.side, poop.speed, poop.side)){
-                poopArray.splice(idxOfFallenPoop[i],1);
+            if(!isInOfCanvas('height', poop.y, poop.size, poop.speed, poop.size)){
+                poopArray.splice(i,1);
+            }
+            if(poop.iscollided(player)){
+                poopArray.splice(i,1);
+                player.damaged();
             }
         }
         addPoop();
